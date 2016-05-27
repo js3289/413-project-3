c200cc1e098c59c20ca29b9c1dcce0265e9a50b6
Joshua Sommers <js3289@nau.edu>
2016-05-26 18:46:51 -0700

    Win conditions fixes + better state handling
    Handled the game state better (Game is no longer playable while in menu,
    etc) and made the win condition visibly by changing camera to gameplay
    container instead of the entire stage.

--- game.js
+++ game.js
@@ -2,7 +2,6 @@
 	var HEIGHT = 500;
 	var WIDTH = 500;
 	var SCALE = 1;
-	var gameState;
 
 // Aliases
 	TextureImage = PIXI.Texture.fromImage;
@@ -33,6 +32,8 @@
 
 	var allowMovement = true;
 
+	var entity_layer;
+	
 // Constants for anchoring sprites
 	var LEFT = 0;
 	var TOP = 0;
@@ -42,7 +43,6 @@
 
 // Misc globals used in game
 	var character;
-	var emptyTile;
 	var keysActive;
 	var tiles;
 	var menu;
@@ -50,12 +50,12 @@
 	var posArr;
 	var slide;
 	var splash;
-	var isAnimating;
+	var gameState;
 	
 	var keysActive = [];
 	var floorData = [];
 	var world;
-	var tu;
+	var tu = new TileUtilities(PIXI);
 	
 	PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
 	
@@ -77,12 +77,6 @@ function handleCollision() {
 		console.log("Collision!");
 	}
 	return hasCollision.hit;
-}
-
-function hasWon() {
-	var won = false;
-	won = tu.hitTestTile(character.boundingSprite, floorData, 5, world, "some").hit;
-	return won
 }
 
 function keepInBounds() {
@@ -111,9 +105,17 @@ function keepInBounds() {
 }
 
 function setup() {
-	tu = new TileUtilities(PIXI);
+	
+	fillContainers();
+	controlState("main");
+	animate();
+}
+
+function createGame() {
+
 	world = tu.makeTiledWorld("map.json", "tileset.png");
 	gameplayC.addChild(world);
+	entity_layer = world.getObject("Entities");
 	slide = PIXI.audioManager.getAudio("TileSlide.mp3");
 	splash = PIXI.audioManager.getAudio("Assets/Sounds/Splash.mp3");
 	
@@ -121,11 +123,7 @@ function setup() {
 	
 	character = new Player("Player", "Assets/png/Character-sprite.png");
 	
-	var entity_layer = world.getObject("Entities");
-	
 	floorData = world.getObject("Floor").data;
-	console.log(floorData);
-	console.log(character);
 	document.addEventListener('keydown', function (e) {
 		keysActive[e.keyCode] = true;
 		e.preventDefault();
@@ -138,24 +136,16 @@ function setup() {
 	
 	entity_layer.addChild(character);
 	entity_layer.addChild(character.boundingSprite);
-	console.log(world.tileheight + " : " + world.tilewidth + " : " + world.widthInTiles);
-	
-	stage.addChild(gameplayC);
-	fillContainers();
-	animate();
+	controlState("play")
+}
+
+function hasWon() {
+	var won = false;
+	won = tu.hitTestTile(character.boundingSprite, floorData, 5, world, "some").hit;
+	return won
 }
 
 function fillContainers() {
-	// Create background. Center background + add it to stage.
-	var background = new Sprite(TextureFrame("background-final.png"));
-	background.anchor.x = MIDDLE;
-	background.anchor.y = MIDDLE;
-	background.position.x = WIDTH / 2;
-	background.position.y = HEIGHT / 2;
-
-// Add background to stage
-	gameplayC.addChild(background);
-
 	
 // Main menu
 	mainMenuC.addChild(new Sprite(TextureFrame("menu.png")));
@@ -168,7 +158,7 @@ function fillContainers() {
 	playButton.y = 150;
 	
 	playButton.interactive = true;
-	playButton.on('mousedown', function() { controlState("play") } );
+	playButton.on('mousedown', function() { createGame() } );
 	
 	mainMenuC.addChild(playButton);
 	
@@ -205,7 +195,7 @@ function fillContainers() {
 	backButton.anchor.x = LEFT;
 	backButton.anchor.y = TOP;
 	backButton.x = 154;
-	backButton.y = 370;
+	backButton.y = 420;
 	
 	
 	backButton.interactive = true;
@@ -228,6 +218,10 @@ function fillContainers() {
 	backButton.on('mousedown', function() { controlState("main") } );
 	
 	creditsC.addChild(backButton);
+	
+// Win
+
+	winC.addChild(new Sprite(TextureFrame("you-win.png")));
 }
 
 function controlState(state) {
@@ -239,25 +233,24 @@ function controlState(state) {
 	
 	if(gameState === "play") {
 		stage.addChild(gameplayC);
-		stage.addChild(puzzleC);
 	}
 	else if(gameState === "main") {
 		stage.addChild(mainMenuC);
-	}
-	else if (gameState === "title"){
-		stage.addChild(titleC);
 	}
 	else if (gameState === "instructions"){
 		stage.addChild(instructionsC);
 	}
 	else if(gameState === "win") {
+		temp = new Sprite(TextureFrame("you-win.png"));
+		temp.x = stage.x;
+		temp.y = stage.y;
+		
+		winC.addChild(temp);
 		stage.addChild(winC);
 	}
-	
 	else if(gameState === "menu") {
 		stage.addChild(menuC);
 	}
-	
 	else if(gameState === "credits") {
 		stage.addChild(creditsC);
 	}
@@ -266,9 +259,11 @@ function controlState(state) {
 
 function animate() {
 	requestAnimationFrame(animate);
-	handleKeys();
-	keepInBounds();
-	updateCamera();
+	if(gameState === "play"){
+		handleKeys();
+		keepInBounds();
+		updateCamera();
+	}
 	renderer.render(stage);
 }
 
@@ -277,7 +272,7 @@ function handleKeys() {
 	yMove = 0;
 	
 	if(hasWon()){
-		alert("you win!");
+		controlState("win");
 	}
 	
 	if (allowMovement) {
@@ -348,17 +343,15 @@ function handleKeys() {
 				character.position.y += yMove;
 				character.boundingSprite.position.y += yMove;
 			}
-			console.log(character.boundingSprite.position.x + " : " + character.boundingSprite.position.y);
-			console.log(character.position.x + " :: " + character.position.y);
 		}
 	}
 }
 
 function updateCamera() {
-	stage.x = -character.x * SCALE + WIDTH / 2 - character.width / 2 * SCALE;
-	stage.y = -character.y * SCALE + HEIGHT / 2 - character.height / 2 * SCALE;
-	stage.x = -Math.max(0, Math.min(world.worldWidth * SCALE - WIDTH, -stage.x));
-	stage.y = -Math.max(0, Math.min(world.worldHeight * SCALE - HEIGHT, -stage.y));
+	gameplayC.x = -character.x * SCALE + WIDTH / 2 - character.width / 2 * SCALE;
+	gameplayC.y = -character.y * SCALE + HEIGHT / 2 - character.height / 2 * SCALE;
+	gameplayC.x = -Math.max(0, Math.min(world.worldWidth * SCALE - WIDTH, -gameplayC.x));
+	gameplayC.y = -Math.max(0, Math.min(world.worldHeight * SCALE - HEIGHT, -gameplayC.y));
 }
 
 /**
